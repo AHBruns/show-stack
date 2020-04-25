802db8878b67071b291bda4a0fcc73e1234b738c
alex <Alex.H.Bruns@Gmail.com>
2020-04-25 23:57:24 +0000

    AHB :: fixing postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,17 +2,22 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+
     ...(process.env.NODE_ENV === "production"
-      ? [
-          [
-            "@fullhuman/postcss-purgecss",
-            {
-              content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
-              defaultExtractor: (content) =>
-                content.match(/[\w-/.:]+(?<!:)/g) || [],
-            },
-          ],
-        ]
-      : []),
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./components/**/*.*",
+              "./pages/**/*.*",
+              "./contexts/**/*.*",
+              "./gql/**/*.*",
+              "./utils/**/*.*",
+              "./public/**/*.*",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/.:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
   },
 };
