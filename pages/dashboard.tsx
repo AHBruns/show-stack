2aace80081f2a67afcfb5d5effec1bd0503a5512
Alex Bruns <alexbruns@Alexs-MacBook-Pro.local>
2020-04-26 17:22:24 -0500

    AHB :: reload bug fix

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import { PrimaryOperationsMenu } from "../components/PrimaryOperationsMenu";
 import { ShowsLayout } from "../components/ShowsLayout";
+import { Router, useRouter } from "next/router";
+import { log } from "../utils/log";
+import UserContext from "../contexts/User.context";
+import {
+  StackContext,
+  actions as stackActions,
+} from "../contexts/StackContext";
 
 export default () => {
+  const router = useRouter();
+
+  const [userState, userDispatch, userOperations] = React.useContext(
+    UserContext
+  );
+  const [_, stackDispatch] = React.useContext(StackContext);
+
+  React.useEffect(() => {
+    userOperations.attemptLoggingInFromLocalStorage(
+      (user) => {
+        stackDispatch(stackActions.SET_ID(user.stack.id));
+      },
+      () => {
+        log(
+          "login",
+          "No user found in localStorage. " +
+            "The user will have to manually enter their credentials."
+        );
+        router.push("/");
+      }
+    );
+  }, []);
+
   return (
     <div className="relative flex-1 w-full max-h-full">
       <div className="absolute inset-0 flex flex-col items-center overflow-scroll bg-gray-900">
