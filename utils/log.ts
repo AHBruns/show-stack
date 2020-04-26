740b5ea3641fdcccfe74688078dcb36d8fe59a42
Alex Bruns <alexbruns@Alexs-MacBook-Pro.local>
2020-04-26 15:58:06 -0500

    AHB :: debug removal

--- utils/log.ts
+++ utils/log.ts
@@ -3,19 +3,19 @@ const senders = {
   login: "LOGIN",
 };
 
-const defaultDebugConfiguration = {
+const debugConfiguration = {
   hideList: [],
 };
 
 export const log = (sender: string, message: string) => {
-  if (window["debugConfiguration"].hideList.includes(sender)) return;
-  console.info(`[${Date.now()}][${window["senders"][sender]}]> ${message}`);
+  if (debugConfiguration.hideList.includes(sender)) return;
+  console.info(`[${Date.now()}][${senders[sender]}]> ${message}`);
 };
 
 // bodge to get this attached to the window object on the client without turning off SSR for everything
-setTimeout(() => {
-  if (typeof window !== "undefined") {
-    window["senders"] = senders;
-    window["debugConfiguration"] = defaultDebugConfiguration;
-  }
-}, 1);
+// setTimeout(() => {
+//   if (typeof window !== "undefined") {
+//     window["senders"] = senders;
+//     window["debugConfiguration"] = defaultDebugConfiguration;
+//   }
+// }, 1);
